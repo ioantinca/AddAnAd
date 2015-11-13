1abb3e2b5117b9625be9107617d9ab8162e9a3c9
itinca <itinca@axway.com>
2015-11-13 15:47:33 +0200

    Testing CategoryRepository

--- src/test/java/com/ioan/repository/RepositoriesTest.java
+++ src/test/java/com/ioan/repository/RepositoriesTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.hamcrest.beans.HasPropertyWithValue;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +19,12 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.ioan.model.Ad;
 import com.ioan.model.Category;
 import com.ioan.repository.CategoryRepository;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,24 +33,52 @@ import static org.junit.Assert.*;
 		DirtiesContextTestExecutionListener.class,
 		TransactionalTestExecutionListener.class,
 		DbUnitTestExecutionListener.class })
-@DatabaseSetup("classpath:categoryData.xml")
-public class CategoryRepositoryTest {
+@DatabaseSetup("classpath:testsData.xml")
+public class RepositoriesTest {
 
 	@Autowired
 	public CategoryRepository categoryRepository;
 
-	@Test
+	@Autowired
+	public AdRepository adRepository;
+
+	 @Test
 	public void search_NoCategoriesFound_ShouldReturnEmptyList() {
 		List<Category> categoryEntries = categoryRepository
 				.findByName("NOT FOUND");
 		assertEquals(categoryEntries.size(), 0);
 	}
-	
+
+	 @Test
 	public void search_OneCategoryFound_ShouldReturnAListOfOneEntry() {
 		List<Category> categoryEntries = categoryRepository
 				.findByName("Autoturisme");
-		assertEquals(categoryEntries.size(), 1);
-//		Category category
+		assertThat(categoryEntries.size(), equalTo(1));
+		assertThat(
+				categoryEntries.get(0),
+				allOf(hasProperty("id", is(2L)),
+						hasProperty("name", is("Autoturisme"))));
+		assertThat(categoryEntries.get(0).getParent(),
+				hasProperty("id", is(1L)));
 	}
 
+	@Test
+	public void search_CategoryByParent() {
+		List<Category> categoryEntries = categoryRepository.findByName("Auto");
+
+		Category parent = categoryEntries.get(0);
+
+		List<Category> childCategories = categoryRepository
+				.findByParent(parent);
+
+		assertThat(childCategories.size(), equalTo(2));
+		assertThat(
+				childCategories,
+				contains(
+						allOf(hasProperty("id", is(2L)),
+								hasProperty("name", is("Autoturisme"))),
+						allOf(allOf(hasProperty("id", is(3L)),
+								hasProperty("name", is("Servicii"))))));
+
+	}
 }
