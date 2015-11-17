0b39cc8ce59c6210d930e7ef3b537c7afbfeca08
itinca <itinca@axway.com>
2015-11-17 15:38:05 +0200

    Unit testing for repository methods

--- src/test/java/com/ioan/repository/RepositoriesTest.java
+++ src/test/java/com/ioan/repository/RepositoriesTest.java
@@ -20,6 +20,7 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.ioan.model.Ad;
+import com.ioan.model.AdKeyword;
 import com.ioan.model.Category;
 import com.ioan.repository.CategoryRepository;
 
@@ -42,6 +43,9 @@ public class RepositoriesTest {
 	@Autowired
 	public AdRepository adRepository;
 
+	@Autowired
+	public AdKeywordRepository adKeywordRepository;
+
 	 @Test
 	public void search_NoCategoriesFound_ShouldReturnEmptyList() {
 		List<Category> categoryEntries = categoryRepository
@@ -62,7 +66,7 @@ public class RepositoriesTest {
 				hasProperty("id", is(1L)));
 	}
 
-	@Test
+	 @Test
 	public void search_CategoryByParent() {
 		List<Category> categoryEntries = categoryRepository.findByName("Auto");
 
@@ -80,5 +84,85 @@ public class RepositoriesTest {
 						allOf(allOf(hasProperty("id", is(3L)),
 								hasProperty("name", is("Servicii"))))));
 
+	}
+
+	 @Test
+	public void searchAd_ByCategoryNull() {
+		List<Ad> adEntries = adRepository.findByCategory(null);
+
+		assertThat(adEntries.size(), equalTo(0));
+	}
+
+	 @Test
+	public void searchAd_ByInexistentCategory() {
+
+		Category inexistentCategory = new Category("INEXISTENT");
+		inexistentCategory.setId(-1L);
+
+		List<Ad> adEntries = adRepository.findByCategory(inexistentCategory);
+
+		assertThat(adEntries.size(), equalTo(0));
+	}
+
+	 @Test
+	public void searchAd_ByExistentCategory() {
+		Category autoturismeCategory = categoryRepository.findOne((long) 2);
+
+		List<Ad> adEntries = adRepository.findByCategory(autoturismeCategory);
+
+		assertThat(adEntries.size(), equalTo(2));
+		assertThat(
+				adEntries,
+				contains(
+						allOf(hasProperty("id", is(1L)),
+								hasProperty("name", is("Mercedes")),
+								hasProperty("description", is("Vand Mercedes")),
+								hasProperty("price", is(1000.0))),
+						allOf(hasProperty("id", is(2L)),
+								hasProperty("name", is("Audi")),
+								hasProperty("description", is("Vand Audi")),
+								hasProperty("price", is(1000.0)))));
+
+	}
+
+	 @Test
+	public void searchAdKeyword_ByInexistentKeyword() {
+		List<AdKeyword> adKeywords = adKeywordRepository
+				.findByKeyword("INEXISTENT");
+
+		assertThat(adKeywords.size(), equalTo(0));
+	}
+
+	@Test
+	public void searchAdKeyowrd_ByExistentKeyWord() {
+		List<AdKeyword> adKeywords = adKeywordRepository
+				.findByKeyword("masina");
+
+		assertThat(adKeywords.size(), equalTo(2));
+		assertThat(
+				adKeywords,
+				contains(
+						allOf(hasProperty("id", is(2L)),
+								hasProperty("keyword", is("masina"))),
+						allOf(hasProperty("id", is(3L)),
+								hasProperty("keyword", is("masina")))));
+	}
+	
+	@Test
+	public void searchAdKeyword_ByAd() {
+		Ad ad = adRepository.findOne(1L);
+		
+		List<AdKeyword> adKeywords = adKeywordRepository.findByAd(ad);
+		
+		assertThat(adKeywords.size(), equalTo(2));
+		assertThat(
+				adKeywords,
+				contains(
+						allOf(hasProperty("id", is(1L)),
+								hasProperty("keyword", is("mercedes"))),
+						allOf(hasProperty("id", is(2L)),
+								hasProperty("keyword", is("masina")))));
+	
+		
 	}
 }
