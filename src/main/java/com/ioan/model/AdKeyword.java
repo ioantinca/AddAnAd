451cccfb25ffba867a543109ad068852078e8e69
itinca <itinca@axway.com>
2015-11-26 18:04:25 +0200

    add Task entity and generationType.AUTO for ids

--- src/main/java/com/ioan/model/AdKeyword.java
+++ src/main/java/com/ioan/model/AdKeyword.java
@@ -2,14 +2,17 @@ package com.ioan.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="adkeyword")
 public class AdKeyword {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String keyword;
 
--- src/main/java/com/ioan/model/Category.java
+++ src/main/java/com/ioan/model/Category.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -14,7 +15,7 @@ import javax.persistence.OneToMany;
 public class Category {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 
