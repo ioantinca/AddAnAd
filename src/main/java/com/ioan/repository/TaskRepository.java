170c4d7179ea7c64cbdbb225704e67b33910548d
itinca <itinca@axway.com>
2015-11-26 18:06:54 +0200

    add taskRepository

---
+++ src/main/java/com/ioan/repository/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.ioan.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ioan.model.Task;
+
+@RepositoryRestResource
+public interface TaskRepository extends CrudRepository<Task, Integer> {
+
+	List<Task> findByTaskArchived(@Param("archivedfalse") int taskArchivedFalse);
+
+	List<Task> findByTaskStatus(@Param("status") String taskStatus);
+
+}
