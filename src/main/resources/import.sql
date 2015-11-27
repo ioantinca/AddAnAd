55026c1a592303b4e411286be3f85927a70fa19c
itinca <itinca@axway.com>
2015-11-27 12:52:25 +0200

    initial data to database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO category VALUES (1, 'Auto', NULL);
+INSERT INTO category VALUES (2, 'Autoturisme', 1);
+INSERT INTO category VALUES (3, 'Servicii', 1);
+INSERT INTO category VALUES (4, 'Piese auto', 3);
+INSERT INTO category VALUES (5, 'Service-uri', 3);
